5eb11656d9e885966336067968289af59f3da3ae
Yves Loeys <yves.loeys@gmail.com>
2021-01-21 11:39:11 -0800

    React router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
-
+import React from "react"
 import './App.css';
+import Navbar from "./components/Navbar"
+import { BrowserRouter as Routes,Route } from 'react-router-dom'
+import Home from "./components/views/Home"
+import Login from "./components/views/Login"
+import Profile from "./components/views/Profile"
+import Signup from "./components/views/Signup"
+
+
 
 function App() {
   return (
-   <h1>Client</h1>
+    <Routes>
+    <Navbar/>
+    
+    <Route path="/login" component={Login}/>
+    <Route path="/profile" component={Profile}/>
+    <Route path="/signup" component={Signup}/>
+    <Route path="/" exact component={Home}/>
+    
+    </Routes>
+  
   );
 }
 
---
+++ client/src/components/views/Signup.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Signup = () => {
+    return (
+        <h1>Signup screen</h1>
+    )
+}
+
+export default Signup
