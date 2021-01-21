4625c2edb5b70ab3835629e89e8c1f61564f4dca
Yves Loeys <yves.loeys@gmail.com>
2021-01-21 13:36:33 -0800

    UI for Homepage

--- client/src/components/views/Home.js
+++ client/src/components/views/Home.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-    return (
-        <h1>Home screen</h1>
-    )
-}
+  return (
+    <div className="home">
+      <div className="card homepage-card">
+        <h5>Sevy Syeol</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1515121061221-7d6ce2dcd1fe?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dmlld3xlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt=""
+          />
+        </div>
+        <i class="material-icons">favorite</i>
 
-export default Home
+        <div className="card-content">
+          <h6>Title</h6>
+          <p>First post</p>
+          <input type="text" placeholder="add comment" />
+        </div>
+      </div>
+
+      <div className="card homepage-card">
+        <h5>Sevy Syeol</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1515121061221-7d6ce2dcd1fe?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dmlld3xlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt=""
+          />
+        </div>
+        <i class="material-icons">favorite</i>
+
+        <div className="card-content">
+          <h6>Title</h6>
+          <p>First post</p>
+          <input type="text" placeholder="add comment" />
+        </div>
+      </div>
+
+      <div className="card homepage-card">
+        <h5>Sevy Syeol</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1515121061221-7d6ce2dcd1fe?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dmlld3xlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt=""
+          />
+        </div>
+        <i class="material-icons">favorite</i>
+
+        <div className="card-content">
+          <h6>Title</h6>
+          <p>First post</p>
+          <input type="text" placeholder="add comment" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
