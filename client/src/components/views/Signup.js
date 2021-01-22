3ca4efd8f47e62728bb5744094c66596b3822a33
Yves Loeys <yves.loeys@gmail.com>
2021-01-21 23:26:10 -0800

    connecting signup to backend+errorfix auth

--- client/src/components/views/Signup.js
+++ client/src/components/views/Signup.js
@@ -1,19 +1,81 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import M from "materialize-css";
 
 const Signup = () => {
+  const history = useHistory();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const postData = () => {
+    if (
+      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+      )
+    ) {
+      M.toast({
+        html: "Invalid email",
+        classes: "#1de9b6 teal accent-3",
+      });
+      return;
+    }
+    fetch("/signup", {
+      method: "post",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        password,
+        email,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          M.toast({
+            html: "Please fill out all the fields",
+            classes: "#e57373 red lighten-2",
+          });
+        } else {
+          M.toast({
+            html: "Signup successfull",
+            classes: "#1de9b6 teal accent-3",
+          });
+          history.push("/signup");
+        }
+      });
+  };
+
   return (
     <div className="mycard">
       <div className="card signup-card">
         <h2>Sign Up</h2>
-        <input type="text" placeholder="name" />
-        <input type="text" placeholder="email" />
-
-        <input type="text" placeholder="password" />
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <button
           className="btn waves-effect waves-light #82b1ff blue accent-1
 "
+          onClick={() => postData()}
         >
           Signup
         </button>
