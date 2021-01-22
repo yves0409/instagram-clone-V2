ab896aeeed143e43a77341bd81ca45298485a2f8
Yves Loeys <yves.loeys@gmail.com>
2021-01-22 13:14:10 -0800

    displaying posts on homepage

--- client/src/components/views/Home.js
+++ client/src/components/views/Home.js
@@ -1,58 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("./allposts", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result.posts);
+        setData(result.posts);
+      });
+  }, []);
   return (
     <div className="home">
-      <div className="card homepage-card">
-        <h5>Sevy Syeol</h5>
-        <div className="card-image">
-          <img
-            src="https://images.unsplash.com/photo-1515121061221-7d6ce2dcd1fe?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dmlld3xlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-            alt=""
-          />
-        </div>
-        <i class="material-icons">favorite</i>
+      {data.map((item) => {
+        return (
+          <div className="card homepage-card">
+            <h5>{item.postedBy.name}</h5>
+            <div className="card-image">
+              <img src={item.photo} alt="pic" />
+            </div>
+            <i class="material-icons">favorite</i>
 
-        <div className="card-content">
-          <h6>Title</h6>
-          <p>First post</p>
-          <input type="text" placeholder="add comment" />
-        </div>
-      </div>
-
-      <div className="card homepage-card">
-        <h5>Sevy Syeol</h5>
-        <div className="card-image">
-          <img
-            src="https://images.unsplash.com/photo-1515121061221-7d6ce2dcd1fe?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dmlld3xlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-            alt=""
-          />
-        </div>
-        <i className="material-icons">favorite</i>
-
-        <div className="card-content">
-          <h6>Title</h6>
-          <p>First post</p>
-          <input type="text" placeholder="add comment" />
-        </div>
-      </div>
-
-      <div className="card homepage-card">
-        <h5>Sevy Syeol</h5>
-        <div className="card-image">
-          <img
-            src="https://images.unsplash.com/photo-1515121061221-7d6ce2dcd1fe?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dmlld3xlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-            alt=""
-          />
-        </div>
-        <i class="material-icons">favorite</i>
-
-        <div className="card-content">
-          <h6>Title</h6>
-          <p>First post</p>
-          <input type="text" placeholder="add comment" />
-        </div>
-      </div>
+            <div className="card-content">
+              <h6>{item.title}</h6>
+              <p>{item.body}</p>
+              <input type="text" placeholder="add comment" />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
