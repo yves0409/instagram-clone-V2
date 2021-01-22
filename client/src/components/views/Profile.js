82cf027c6ff1ea3d3e55f08ca4f4c38308fd5204
Yves Loeys <yves.loeys@gmail.com>
2021-01-22 14:17:33 -0800

    show own posts on profile

--- client/src/components/views/Profile.js
+++ client/src/components/views/Profile.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { UserContext } from "../../App";
 
 const Profile = () => {
+  const [myposts, setMyposts] = useState([]);
+  const { state, dispatch } = useContext(UserContext);
+  useEffect(() => {
+    fetch("./myposts", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setMyposts(result.mypost);
+      });
+  }, []);
   return (
     <div style={{ maxWidth: "600px", margin: "0px auto" }}>
       <div
@@ -23,80 +37,28 @@ const Profile = () => {
           />
         </div>
         <div>
-          <h4>Sevy Syeol </h4>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            width: "108%",
-            margin: "10px",
-          }}
-        >
-          <h6>36 posts</h6>
-          <h6>20k followers</h6>
-          <h6>640 following</h6>
+          <h4>{state ? state.name : "loading"} </h4>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "108%",
+              margin: "10px",
+            }}
+          >
+            <h6>36 posts</h6>
+            <h6>20k followers</h6>
+            <h6>640 following</h6>
+          </div>
         </div>
       </div>
       <div className="post-gallery">
-        <img
-          className="post-item"
-          src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
-          alt=""
-        />
-        <img
-          className="post-item"
-          src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
-          alt=""
-        />
-
-        <img
-          className="post-item"
-          src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
-          alt=""
-        />
-
-        <img
-          className="post-item"
-          src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
-          alt=""
-        />
-
-        <img
-          className="post-item"
-          src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
-          alt=""
-        />
-
-        <img
-          className="post-item"
-          src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
-          alt=""
-        />
-
-        <img
-          className="post-item"
-          src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
-          alt=""
-        />
-
-        <img
-          className="post-item"
-          src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
-          alt=""
-        />
-
-        <img
-          className="post-item"
-          src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
-          alt=""
-        />
-
-        <img
-          className="post-item"
-          src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
-          alt=""
-        />
+        {myposts.map((item) => {
+          return (
+            <img className="post-item" key={item._id} src={item.photo} alt="" />
+          );
+        })}
       </div>
     </div>
   );
