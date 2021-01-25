2e01bb294c381c2ac1e0e683f1f3ebdffcf5cde9
Yves Loeys <yves.loeys@gmail.com>
2021-01-25 08:37:32 -0800

    follow/unfollow routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -22,7 +22,66 @@ router.get("/user/:id", loginMiddleware, (req, res) => {
     .catch((err) => {
       return res.status(404).json({ error: "User Not Found" });
     });
-  //console.log(user);
+});
+
+//FOLLOW ROUTE
+
+router.put("/follow", (req, res) => {
+  User.findByIdAndUpdate(
+    req.body.followId,
+    {
+      $push: { follower: req.user._id },
+    },
+    { new: true },
+    (err, result) => {
+      if (err) {
+        res.status(422).json({ err: error });
+      }
+    }
+  );
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $push: { following: req.body.followId },
+    },
+    { new: true }
+  )
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      return res.status(422).json({ error: err });
+    });
+});
+
+//UNFOLLOW ROUTE
+
+router.put("/unfollow", (req, res) => {
+  User.findByIdAndUpdate(
+    req.body.unfollowId,
+    {
+      $pull: { follower: req.user._id },
+    },
+    { new: true },
+    (err, result) => {
+      if (err) {
+        res.status(422).json({ err: error });
+      }
+    }
+  );
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $pull: { following: req.body.unfollowId },
+    },
+    { new: true }
+  )
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      return res.status(422).json({ error: err });
+    });
 });
 
 module.exports = router;
